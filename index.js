b9ea17a7e3b9eb8c666da11021ccd86de5053d05
ben40d9 <benpaley6@gmail.com>
2022-11-16 03:33:41 -0800

    test channel

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //test
+import fetch from "node-fetch";
+
+const item = fetch("https://59c130.myshopify.com/admin", {
+  headers: {
+    "Content-Type": "application.json",
+  },
+});
+console.log(item);
